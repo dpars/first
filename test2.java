aab5e7aa2b6537ca9ec9f038c04597fab3b371de
dpars <test1.sync@yandex.ru>
2017-01-05 10:54:41 +0200

    third commit

---
+++ test2.java
@@ -0,0 +1,10 @@
+/**
+ * Created by parshikov on 05.01.2017.
+ */
+public class test2 {
+    public static void  main(String[] arg){
+        System.out.println("dsf");
+        System.out.println("commit2");
+    }
+
+}
